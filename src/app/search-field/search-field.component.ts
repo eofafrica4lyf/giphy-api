13dccb4d4a8d7a3ce259ca75ac07a3405984596d
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2020-01-08 13:30:08 +0100

    implement service to pull gif data

--- src/app/search-field/search-field.component.ts
+++ src/app/search-field/search-field.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IGifs } from '../interfaces';
+import { ADD_GIFS } from '../actions';
+import { GiphyApiService } from '../services/giphy-api.service';
 
 @Component({
     selector: 'app-search-field',
-    // selector: 'app-root',
     templateUrl: './search-field.component.html',
     styleUrls: ['./search-field.component.css']
 })
 export class SearchFieldComponent implements OnInit {
-    name = 'Search Field';
-    constructor() {
+		name = 'Search Field';
+		
+    constructor(private apiService: GiphyApiService ) {
     }
 
     ngOnInit() {
-    }
+		}
+		
+		onSubmit() {
+			this.apiService.getImages().subscribe((res) => {
+				console.log(res);
+			})
+		}
 }
---
+++ src/app/services/giphy-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GiphyApiService {
+	API_KEY = 'it1PwMOxEAo9sH6qlZRKCmpt6KJNmkj0';
+	
+	constructor(private httpClient: HttpClient) { }
+	
+	public getImages(){
+		return this.httpClient.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.API_KEY}&q=sphinx&limit=25&offset=0&rating=G&lang=en`)
+	}
+}
